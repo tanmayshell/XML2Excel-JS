db97ea25ab6b15608ded896f72d5b18d4598cfec
lakshya317 <lakshya.sharma@shell.com>
2023-10-03 15:54:09 +0530

    Add Async Await

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import { convertXmlToExcel } from './xmlToExcel.js';
 const xmlFilePath = 'task.xml';
-const excelFilePath = 'task.xlsx';
+const excelFilePath = 'task1.xlsx';
 
-convertXmlToExcel(xmlFilePath, excelFilePath, (err, result) => {
-  if (err) {
-    console.error('Unexpected Error happened', err);
-  } else {
-    console.log('result', result);
-  }
-});
+convertXmlToExcel(xmlFilePath, excelFilePath);
--- xmlToExcel.js
+++ xmlToExcel.js
@@ -3,64 +3,43 @@ import xml2js from 'xml2js';
 import xlsx from 'xlsx';
 
 // Function to convert XML data to Excel
-export function convertXmlToExcel(xmlFilePath, excelFilePath, cb) {
-  // Read XML file
-  fs.readFile(xmlFilePath, 'utf-8', (err, xmlData) => {
-    if (err) {
-      cb(err);
-      return;
-    }
+export const convertXmlToExcel = async (xmlFilePath, excelFilePath) => {
+  try {
+    // Read XML file
+    const xmlData = await fs.promises.readFile(xmlFilePath, 'utf-8');
     // Parse XML to JS object
     const parser = new xml2js.Parser({
       explicitArray: false,
       mergeAttrs: true,
     });
     parser.parseString(xmlData, (err, jsObject) => {
-      if (err) {
-        cb(err);
-        return;
-      }
+      //Define movies and series data from the parsed JavaScript object
+      const moviesData = jsObject.binge.movies.movie;
+      const seriesData = jsObject.binge.series.serie;
 
-      // Function to add formatting
-      function formatRange(sheet, range, style) {
-        for (let row = range.s.r; row <= range.e.r; row++) {
-          for (let col = range.s.c; col <= range.e.c; col++) {
-            const reference = xlsx.utils.encode_cell({ r: row, c: col });
-            if (!sheet[reference]) continue;
-            sheet[reference].s = style;
-          }
-        }
-      }
+      // Create worksheets for movies and series
+      const moviesWs = xlsx.utils.json_to_sheet(moviesData);
+      const seriesWs = xlsx.utils.json_to_sheet(seriesData);
 
-      try {
-        //Define movies and series data from the parsed JavaScript object
-        const moviesData = jsObject.binge.movies.movie;
-        const seriesData = jsObject.binge.series.serie;
+      // Create new Workbook
+      const wb = xlsx.utils.book_new();
 
-        // Create worksheets for movies and series
-        const moviesWs = xlsx.utils.json_to_sheet(moviesData);
-        const seriesWs = xlsx.utils.json_to_sheet(seriesData);
+      // Add worksheets to workbook
+      xlsx.utils.book_append_sheet(wb, moviesWs, 'Movies');
+      xlsx.utils.book_append_sheet(wb, seriesWs, 'Series');
 
-        // Create new Workbook
-        const wb = xlsx.utils.book_new();
+      // Define cell style
+      const bold = { font: { bold: true } };
 
-        // Add worksheets to workbook
-        xlsx.utils.book_append_sheet(wb, moviesWs, 'Movies');
-        xlsx.utils.book_append_sheet(wb, seriesWs, 'Series');
+      // Apply formatting
+      const headerRange = xlsx.utils.decode_range(moviesWs['!ref']);
+      // formatRange(moviesWs, headerRange, bold);
 
-        // Define cell style
-        const bold = { font: { bold: true } };
-
-        // Apply formatting
-        const headerRange = xlsx.utils.decode_range(moviesWs['!ref']);
-        formatRange(moviesWs, headerRange, bold);
-
-        // Save the workbook
-        xlsx.writeFile(wb, excelFilePath);
-        cb(null, 'Success');
-      } catch (error) {
-        cb(error);
-      }
+      // Save the workbook
+      xlsx.writeFile(wb, excelFilePath);
+      console.log('Successfully converted XML to Excel');
     });
-  });
-}
+  } catch (error) {
+    console.log("Error in Excel conversion: ", error);
+  }
+};
