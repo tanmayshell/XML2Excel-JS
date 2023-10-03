b8253532c183a794df5ec59e2cd9c013f7e4a647
tanmayshell <146022160+tanmayshell@users.noreply.github.com>
2023-10-03 12:53:03 +0530

    added comments

--- xmlToExcel.js
+++ xmlToExcel.js
@@ -2,12 +2,15 @@ import fs from 'fs'
 import xml2js from 'xml2js'
 import xlsx from 'xlsx'
 
+// Function to convert XML data to Excel
 export function convertXmlToExcel(xmlFilePath, excelFilePath, cb){
+    // Read XML file
     fs.readFile(xmlFilePath, 'utf-8', (err, xmlData) => {
         if(err){
             cb(err)
             return
         }
+        // Parse XML to JS object
         const parser= new xml2js.Parser({explicitArray: false, mergeAttrs:true});
         parser.parseString(xmlData, (err, jsObject)=>{
             if(err){
@@ -16,12 +19,18 @@ export function convertXmlToExcel(xmlFilePath, excelFilePath, cb){
             }
 
             try {
+                //Define movies and series data from the parsed JavaScript object
                 const moviesData=jsObject.binge.movies.movie
                 const seriesData=jsObject.binge.series.serie
+
+                // Create worksheets for movies and series
                 const moviesWs = xlsx.utils.json_to_sheet(moviesData);
                 const seriesWs=xlsx.utils.json_to_sheet(seriesData);              
+                
+                // Create new Workbook
                 const wb = xlsx.utils.book_new();
-
+                
+                // Function to add formatting
                 function formatRange(sheet, range, style){
                     for(let row=range.s.r; row<=range.e.r; row++){
                         for(let col=range.s.c; col<=range.e.c; col++){
@@ -34,14 +43,18 @@ export function convertXmlToExcel(xmlFilePath, excelFilePath, cb){
                 }
 
 
-
+                // Add worksheets to workbook
                 xlsx.utils.book_append_sheet(wb, moviesWs, "Movies");
                 xlsx.utils.book_append_sheet(wb, seriesWs, "Series");
 
+                // Define cell style
                 const bold= {font:{bold:true}}
+
+                // Apply formatting
                 const headerRange=xlsx.utils.decode_range(moviesWs['!ref'])
                 formatRange(moviesWs, headerRange, bold)
-
+                
+                // Save the workbook
                 xlsx.writeFile(wb, excelFilePath)
                 cb(null, 'Success')
             }
